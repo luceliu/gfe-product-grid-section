3416f988b26619673bbc2635632ae6409e28249d
Luce Liu <thisisluce@gmail.com>
2025-02-12 00:35:36 -0500

    start challenge

---
+++ app/components/ProductGridSection.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+interface IProductGridSectionProps {}
+
+const ProductGridSection: React.FunctionComponent<IProductGridSectionProps> = (
+  props
+) => {
+  return <div>{/* stuff */}</div>;
+};
+
+export default ProductGridSection;
